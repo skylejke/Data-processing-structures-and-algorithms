626e42b5f99030d5e0461dadb489a5ec8ed7c7dd
Daniil Damarad <105396608+skylejke@users.noreply.github.com>
2022-12-20 21:38:03 +0300

    Added Task №2 in 2 part

---
+++ 2 part/Task №2/binfile/binfile.cpp
@@ -0,0 +1,319 @@
+﻿#include <string.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#define _CRT_SECURE_NO_WARNINGS 
+using namespace std;
+
+struct Product {
+    char name[20];
+    char manufacturer[20];
+    char country[20];
+    int price;
+    int code;
+};
+Product product;
+
+int inputMenu(int& number) {
+    cout << "Введите номер задания" << endl;
+    cout << "1) Преобразование тестовых данных из текстового файла в двоичный файл" << endl;
+    cout << "2) Преобразование данных из двоичного файла в текстовый" << endl;
+    cout << "3) Вывод всех записей двоичного файла" << endl;
+    cout << "4) Доступ к записи по ее порядковому номеру в файле, используя механизм прямого доступа к записи в двоичном файле" << endl;
+    cout << "5) Удаление записи с заданным значением ключа, выполнить путем замены на последнюю запись" << endl;
+    cout << "6) Сформировать текстовый файл о поставках заданной страны, размещая на строке все сведения о товаре, кроме страны" << endl;
+    cout << "7) Добавить новую запись о товаре в конец файла" << endl;
+    cout << "0) Выход из программы" << endl;
+    while (1) {
+        cout << "Введите число от 0 до 7: ";
+        if ((cin >> number).good() && (number >= 0) && (number <= 7)) {
+            return number;
+        }
+        if (cin.fail()) {
+            cin.clear();
+            cout << endl << "Неверный ввод, повторите." << endl << endl;
+        }
+        else {
+            cout << endl << "Число вне допустимого диапазона значений. Повторите ввод." << endl << endl;
+        }
+        cin.ignore(100, '\n');
+    }
+}
+
+void ConvertTextToBin(string bin_file_name, string txt_file_name) {
+    ifstream fin(txt_file_name, ios::in);
+    if (fin.is_open()) {
+        ofstream fout(bin_file_name, ios::out | ios::binary);
+        Product product;
+        while (!fin.eof()) {
+            fin >> product.name >> product.code >> product.manufacturer >> product.price >> product.country;
+            fout.write((char*)&product, sizeof(Product));
+        }
+        fout.close();
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    fin.close();
+}
+
+void ConvertBinToText(string bin_file_name, string txt_file_name) {
+    ifstream fin;
+    fin.open(bin_file_name, ios::binary | ios::in);
+    if (fin.is_open()) {
+        ofstream fout;
+        fout.open(txt_file_name, ios::out);
+        Product product;
+        while (fin.read((char*)&product, sizeof(Product))) {
+            fout << product.name << " " << product.code << " " << product.manufacturer << " " << product.price << " " << product.country << "\n";
+        }
+        fout.close();
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    fin.close();
+}
+
+void OutputBinFile(string bin_file_name) {
+    ifstream fin(bin_file_name, ios::in | ios::binary);
+    Product product;
+    if (fin.is_open()) {
+        while (fin.read((char*)&product, sizeof(Product))) {
+            cout << product.name << " " << product.code << " " << product.manufacturer << " " << product.price << " " << product.country << "\n";
+        }
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    fin.close();
+}
+
+bool CheckIndex(string bin_file_name, int index) {
+    ifstream fin;
+    fin.open(bin_file_name, ios::binary | ios::in);
+    Product temp;
+    int count = 0;
+    while (fin.read((char*)&temp, sizeof(Product))){
+        count++;
+    }
+    fin.close();
+    if (count > index) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+string GetProduct(string bin_file_name, int index) {
+    ifstream fin;
+    fin.open(bin_file_name, ios::binary | ios::in);
+    string result = "";
+    if (fin.is_open()) {
+        if (!CheckIndex(bin_file_name, index)) {
+            return "Индекс превосходит количество элементов";
+        }
+        Product product;
+        fin.seekg(index * sizeof(Product), ios::beg);
+        fin.read((char*)&product, sizeof(Product));
+        result = product.name;
+        result += " ";
+        result += to_string(product.code);
+        result += " ";
+        result += product.manufacturer;
+        result += " ";
+        result += to_string(product.price);
+        result += " ";
+        result += product.country;
+    }
+    else {
+        result = "Файла с таким именем не существует";
+    }
+    return result;
+    fin.close();
+}
+
+void DeleteRecord(string name, int index){
+    Product product;
+    Product empty;
+    fstream file(name, ios_base::binary | ios::in | ios::out);
+
+    if (file.is_open()) {
+        if (!CheckIndex(name, index)) {
+            cout << "Индекс превышает количество записей" << endl;
+            return;
+        }
+
+        file.seekg((-1) * (int)sizeof(Product), ios::end);
+        file.read((char*)&product, sizeof(Product));
+
+        file.seekg((-1) * (int)sizeof(Product), ios::end);
+        file.write((char*)&empty, sizeof(Product));
+
+        file.seekg((index) * sizeof(Product), ios::beg);
+        file.write((char*)&product, sizeof(Product));
+    }
+    else { 
+        cout << "Файла с таким именем не существует\n"; 
+        return; 
+    } 
+
+    if (file.good()) {
+        file.close();
+    }
+}
+
+bool checkCountry(char* m1, char* m2,int size) {
+    for (int i = 0; i < size; i++) {
+        if (m1[i] != m2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void AdditionalTask1(string bin_file_name, string new_file_name, char country[20]) {
+    ifstream file(bin_file_name, ios_base::binary);
+    if (!file) {
+        cout << "Файла с таким именем не существует\n";
+        return;
+    }
+    ofstream products(new_file_name, ios_base::binary);
+    Product product;
+    file.read((char*)&product, sizeof(Product));
+    products << country << "\n";
+    string temp = country;
+    while (!file.eof()) {
+        if (checkCountry(product.country, country, temp.size())) {
+            products << product.name << " " << product.code << " " << product.manufacturer << " " << product.price << "\n";
+        }
+        file.read((char*)&product, sizeof(Product));
+    }
+    if (file.good()) {
+        file.close();
+    }
+    if (products.good()) {
+        products.close();
+    }
+}
+
+
+void AdditionalTask2(string bin_file_name) {
+    ofstream binfout;
+    binfout.open(bin_file_name, ios::app | ios::binary);
+    Product product;
+    if (binfout.good()) {
+        cout << "Название продукта: "; cin >> product.name; cin.ignore(); cin.clear();
+        cout << "Код продукта: "; cin >> product.code; cin.ignore(); cin.clear();
+        cout << "Производитель продукта: "; cin >> product.manufacturer; cin.ignore(); cin.clear();
+        cout << "Цена продукта: "; cin >> product.price; cin.ignore(); cin.clear();
+        cout << "Страна-производитель продукта: "; cin >> product.country; cin.ignore(); cin.clear();
+        binfout.write((char*)&product, sizeof(Product));
+        binfout.close();
+    }
+    else {
+        cout << "Файла с таким именем не существует\n";
+        return;
+    }
+}
+
+int main() {
+    system("chcp 1251 > null");
+    string bin_file_name;
+    int menu;
+    while (true) {
+        inputMenu(menu);
+        if (menu == 0) {
+            cout << "Задание не выбрано\nВыход из программы..." << endl;
+            break;
+        }
+        cout << endl;
+        switch (menu) {
+        case 1: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            string txt_file_name;
+            cout << "Введите имя текстового файла для копирования в двоичный: ";
+            cin >> txt_file_name;
+            ConvertTextToBin(bin_file_name, txt_file_name);
+            break;
+        }
+        case 2: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            string txt_file_name;
+            cout << "Введите имя текстового файла для копирования из двоичного: ";
+            cin >> txt_file_name;
+            ConvertBinToText(bin_file_name, txt_file_name);
+            break;
+        }
+        case 3: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            OutputBinFile(bin_file_name);
+            break;
+        }
+        case 4: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            int index;
+            cout << "Введите индекс элемента: ";
+            cin >> index;
+            cout << GetProduct(bin_file_name, index) << endl;
+            break;
+        }
+        case 5: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            int index;
+            cout << "Введите индекс элемента, который надо удалить: ";
+            cin >> index;
+            DeleteRecord(bin_file_name, index);
+            break;
+        }
+        case 6: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            char country[20]; cout << "Страна (на английском языке): ";
+            cin >> country;
+            cin.ignore();
+            cin.clear();
+            AdditionalTask1(bin_file_name, "info.bin", country);
+            break;
+        }
+        case 7: {
+            cout << "Введите имя бинарного файла: ";
+            cin >> bin_file_name;
+            cin.ignore();
+            cin.clear();
+            AdditionalTask2(bin_file_name);
+            break;
+        }
+        default: {
+            cout << "Задание не выбрано\nВыход из программы..." << endl;
+            break;
+        }
+        }
+    }
+}
+
+//Тестовые данные для 7-ого задания
+//Apple
+//101111
+//Apple_Factory
+//5
+//Egypt
---
+++ 2 part/Task №2/textfile/textfile.cpp
@@ -0,0 +1,245 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int inputMenu(int& number) {
+    cout << "Введите номер задания" << endl;
+    cout << "1) Cоздание текстового файла кодировки ASCII, содержащего десятичные числа по несколько чисел на строке" << endl;
+    cout << "2) Вывод содержимого текстового файла" << endl;
+    cout << "3) Добавление новой строки в конец файла" << endl;
+    cout << "4) Прочитать значение числа, указав его порядковый номер в файле, и вернуть его значение" << endl;
+    cout << "5) Определить количество чисел в файле" << endl;
+    cout << "6) Создать новый файл из значений исходного, умножив каждое число на сумму первого и последнего чисел исходного файла" << endl;
+    cout << "0) Выход из программы" << endl;
+    while (1) {
+        cout << "Введите число от 0 до 6: ";
+        if ((cin >> number).good() && (number >= 0) && (number <= 6)) {
+            return number;
+        }
+        if (cin.fail()) {
+            cin.clear();
+            cout << endl << "Неверный ввод, повторите." << endl << endl;
+        }
+        else {
+            cout << endl << "Число вне допустимого диапазона значений. Повторите ввод." << endl << endl;
+        }
+        cin.ignore(100, '\n');
+    }
+}
+
+void CreateAndFillFile(string fileName) {
+    ofstream fout;
+    fout.open(fileName, ios::out);
+    int lines = rand() % 10 + 1;
+    for (int i = 0; i < lines; i++) {
+        int countNumbers = rand() % 6 + 1;
+        for (int j = 0; j < countNumbers; j++) {
+            fout << rand() % 20 + 1 << " ";
+        }
+        if (i < lines - 1) {
+            fout << endl;
+        }
+    }
+    if (fout.good()) {
+        fout.close();
+    }
+    else {
+        return;
+    }
+}
+
+void PrintFile(string fileName) {
+    ifstream fin;
+    fin.open(fileName, ios::in);
+    if (fin.is_open()) {
+        cout << "Содержимое файла " << fileName << ":" << "\n";
+        string line;
+        while (!fin.eof()) {
+            getline(fin, line);
+            cout << line << endl;
+        }
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    if (fin.good()) {
+        fin.close();
+    }
+    else {
+        return;
+    }
+}
+
+void AppendLine(string fileName, string line) {
+    ofstream fout;
+    fout.open(fileName, ios::app);
+    fout << endl << line;
+    if (fout.good()) {
+        fout.close();
+    }
+    else {
+        return;
+    }
+}
+
+int GetNumber(string fileName) {
+    int resultNumber = 0;
+    ifstream fin;
+    fin.open(fileName, ios::in);
+    if (fin.is_open()) {
+        int numberIndex;
+        cout << "Номер искомого числа, начиная с 0: ";
+        cin >> numberIndex;
+        for (int i = 0; i <= numberIndex; i++) {
+            fin >> resultNumber;
+        }
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    if (fin.good()) {
+        fin.close();
+    }
+    else {
+        return -1;
+    }
+    return resultNumber;
+}
+
+int CountNumbers(string fileName){
+    int n, count = 0;
+    ifstream fin;
+    fin.open(fileName, ios::in);
+    if (fin.is_open()) {
+        while (fin >> n) {
+            count++;
+        }
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+        return -1;
+    }
+    if (fin.good()) {
+        fin.close();
+    }
+    return count;
+}
+
+void AdditionalTask(string fileName) {
+    ifstream fin;
+    fin.open(fileName, ios::in);
+    if (fin.is_open()) {
+        int temp, n, i = 0, size = CountNumbers(fileName);
+        int* numbers = new int[size];
+        while (fin >> n) {
+            numbers[i] = n;
+            i++;
+        }
+        fin.close();
+        int multiplier = numbers[0] + numbers[size-1];
+        ofstream fout;
+        fout.open("new_file.txt", ios::out);
+        fin.open(fileName, ios::in);
+        while (fin >> temp) {
+            fout << temp * multiplier << " ";
+        }
+        if (fin.good() && fout.good()){
+            fout.close();
+            fin.close();
+        }
+        else {
+            return;
+        }
+    }
+    else {
+        cout << "Файла с таким именем не существует" << endl;
+    }
+    fin.close();
+}
+
+int main() {
+    system("chcp 1251 > null");
+    int menu;
+    string fileName;
+    while (true) {
+        inputMenu(menu);
+        if (menu == 0) {
+            cout << "Задание не выбрано\nВыход из программы..." << endl;
+            break;
+        }
+        cout << endl;
+        switch (menu) {
+        case 1: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            CreateAndFillFile(fileName);
+            break;
+        }
+        case 2: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            PrintFile(fileName);
+            cout<< endl;
+            break;
+        }
+        case 3: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            string lineToAppend;
+            cout << "Введите строку, которую хотите добавить: ";
+            cin.get();
+            getline(cin, lineToAppend);
+            AppendLine(fileName, lineToAppend);
+            break;
+        }
+        case 4: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            int result = GetNumber(fileName);
+            if (result == -1) {
+                cout << "Число не может быть найдено" << endl << endl;
+            }
+            else {
+                cout << "Число под требуемым индексом: " << result << endl << endl;
+            }
+            break;
+        }
+        case 5: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            int count = CountNumbers(fileName);
+            if (count == -1) {
+                cout << endl;
+            }
+            else {
+                cout << "Количество чисел в файле: " << count << endl << endl;
+            }
+            break;
+        }
+        case 6: {
+            cout << "Введите имя файла: ";
+            cin >> fileName;
+            cin.ignore();
+            cin.clear();
+            AdditionalTask(fileName);
+            break;
+        }
+        default: {
+            cout << "Задание не выбрано\nВыход из программы..." << endl;
+            break;
+        }
+        }
+    }
+    return 0;
+}
